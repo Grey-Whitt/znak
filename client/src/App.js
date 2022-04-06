f07a52dde22a2cc84f6e5b21d8c8184435c75018
greyw <greywhitt@gmail.com>
2022-04-06 11:51:47 -0500

    added react router to server homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main>
-        <h1>Znak</h1>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
