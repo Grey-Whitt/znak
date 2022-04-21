19950e76b39e0ea3c4966b01366addfe5d30d9a2
greyw <greywhitt@gmail.com>
2022-04-21 18:26:18 -0500

    changed route to homesreen

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen />} />
+            <Route path='/znak' element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
